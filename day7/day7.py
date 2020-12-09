a92afe34c9c8837de20820c74677db5dc701157b
Kiin <kiindev12@gmail.com>
2020-12-09 15:21:48 +0000

    Day 7 pt1

--- day7/day7.py
+++ day7/day7.py
@@ -1,6 +1,6 @@
-file1 = open('day7.txt', 'r') 
-Lines = file1.readlines() 
-  
+file1 = open('day7.txt', 'r')
+Lines = file1.readlines()
+
 def part1():
     count = 0
     par = ["shiny gold"]
@@ -8,8 +8,8 @@ def part1():
     while(search):
         mark = True
         for bag in par:
-            for line in Lines: 
-                if bag in line: 
+            for line in Lines:
+                if bag in line:
                     toIn  = line[0:line.find("bags")-1];
                     if toIn not in par:
                         par.append(toIn)
@@ -19,20 +19,11 @@ def part1():
             search = False;
     return len(par)-1
 
-def part2():
-    for line in Lines:
-        if line[0:line.find("bags")-1] == "shiny gold":
-            start = line[line.find("contain")+8:].replace("bags","")
-    
-    for bag in start
-
-    print(start)
-    
 
 def main():
     print("Advent of Code!")
     print(part1())
-    part2()
+
 if __name__ == "__main__":
     main()
 
