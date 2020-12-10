fe608c2e7386028c3ed8eacfcb680999a34f4a88
Kiin <kiindev12@gmail.com>
2020-12-10 10:03:13 +0000

    Day9 update

--- day9/day9.py
+++ day9/day9.py
@@ -4,45 +4,35 @@ Lines = inp.readlines()
 def findIT(pt):
     start = 0
     end = 25
-    per = 25
-    goto = Lines[per]
+    goto = int(Lines[end])
     while(True):
         finish = True
         sample = Lines[start:end]
         for v in sample:
             for v2 in sample:
                 if v != v2:
-                    if int(v) + int(v2) == int(goto):
+                    if int(v) + int(v2) == goto:
                         finish = False
-
         if finish:
             if pt == 1:
-                return int(goto)
-            incr = 0
+                return goto
             leng = len(Lines)
             lol = []
-            for i in range(incr, leng, 1):
+            for i in range(0, leng, 1):
                 count = int(Lines[i])
                 lol.append(int(Lines[i]))
                 for p in range((i+1), leng, 1):
                     count+=int(Lines[p])
                     lol.append(int(Lines[p]))
-                    if count > int(goto):
-                        incr+=1
-                        i = incr
+                    if count > goto:
                         lol = []
-                    if count == int(goto):
+                    if count == goto:
                         lol.sort()
                         return lol[0] + lol[len(lol)-1]
-
         else:
-            per+=1
-            goto = Lines[per]
+            end+=1
+            goto = int(Lines[end])
             start += 1
-            if end+1 < len(Lines)-1:
-                end+=1
-            else:
-                end += 1
 
 
 def main():
